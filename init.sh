0eeaa0b1aade363a0f78f68bc0d72a1ab1c0f752
iozheg <mozg@eml.ru>
2017-04-20 22:19:09 +0500

    modified nginx.conf and init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s etc/nginx/nginx.conf //etc/nginx/sites-enabled/my.conf
+sudo ln -s etc/nginx/nginx.conf //etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
