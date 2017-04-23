499353d0e033537f94a118cd2285036672d3462a
iozheg <mozg@eml.ru>
2017-04-23 23:43:36 +0500

    init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx/nginx.conf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/gunicornconfig.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicornconf.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
 gunicorn -c /etc/gunicorn.d/hello.py hello:app
