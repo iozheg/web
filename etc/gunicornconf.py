4f4f34bcb0dc8b5cd31f5bd9c52f67baee1aba56
iozheg <mozg@eml.ru>
2017-04-23 22:56:56 +0500

    modified nginx.conf and init.sh

---
+++ etc/gunicornconf.py
@@ -0,0 +1,5 @@
+bind = "0.0.0.0:8080"
+
+accesslog = "/home/box/web/gunicorn_access_log"
+errorlog = "/home/box/web/gunicorn_error_log"
+loglevel = "debug"
---
+++ hello.py
@@ -0,0 +1,9 @@
+def app(env, start_response)
+	data = env["QUERY_STRING"].split("&")
+	status = '200 OK'
+	headers = [
+		('Content-Type', 'text/plain')
+	]
+
+	start_response(status, headers)
+	return data
