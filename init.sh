a65c1640db5b17a6e2d7b394a68e35be668fd995
iozheg <mozg@eml.ru>
2017-04-23 23:05:33 +0500

    created gunicorn conf, WSGI app, modified init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/gunicornconfig.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/gunicorn restart
