64dc7a887e4c4d216e81c791be68692fee4b5708
iozheg <mozg@eml.ru>
2017-04-24 00:06:17 +0500

    add bytes() in hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def app(env, start_response):
 	data = env["QUERY_STRING"].split("&")
-	output = [entry + '\n' for entry in data]
+	output = [bytes(entry + '\n', 'ascii') for entry in data]
 	status = '200 OK'
 	headers = [
 		('Content-Type', 'text/plain')
