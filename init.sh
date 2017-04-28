ca5cf8dfc4a2c99429e4b1f0f671f295e7e06c53
iozheg <mozg@eml.ru>
2017-04-28 20:43:46 +0500

    removed gunicorn auto start

--- init.sh
+++ init.sh
@@ -3,4 +3,3 @@ sudo ln -sf /home/box/web/etc/nginx/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/gunicornconf.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
-gunicorn -c /etc/gunicorn.d/hello.py hello:app
