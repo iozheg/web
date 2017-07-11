b5ef8068f702288254744964d18f6f6b608319fc
alex <mozg@eml.ru>
2017-07-11 02:23:48 -0700

    config nginx and unicorn

--- etc/gunicornconf.py
+++ etc/gunicornconf.py
@@ -1,6 +1,6 @@
 bind = "0.0.0.0:8080"
 bind = "0.0.0.0:8000"
 
-accesslog = "-"
-errorlog = "-"
+accesslog = "/home/box/gunicorn_access.log"
+errorlog = "/home/box/gunicorn_error.log"
 loglevel = "debug"
---
+++ gunicornconf_home.py
@@ -0,0 +1,6 @@
+bind = "127.0.0.1:8080"
+bind = "127.0.0.1:8000"
+
+accesslog = "/usr/projects/gunicorn_access.log"
+errorlog = "/usr/projects/gunicorn_error.log"
+loglevel = "debug"
